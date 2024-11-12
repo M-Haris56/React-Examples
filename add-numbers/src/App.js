2ddfd34854526794cca0ab45717cdad2e993d20e
Muhammad Haris <mharis5659526@gmail.com>
2024-11-13 01:16:07 +0500

    Add-Numbers

---
+++ add-numbers/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let a = 1;
+  let b = 2;
+  let c = a + b;
+  return (
+    <>
+      <div>{c}</div>
+    </>
+  );
+}
+
+export default App;

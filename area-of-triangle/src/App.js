0ed2ecee5d4d852f0d2f4ffae84dc23acae40799
Muhammad Haris <mharis5659526@gmail.com>
2024-11-13 22:28:36 +0500

    area of triangle

---
+++ area-of-triangle/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let b = 23;
+  let h = 35;
+  let area = 1 / 2 * (b * h);
+  return (
+    <>
+      <div>{area}</div>
+    </>
+  );
+}
+
+export default App;
